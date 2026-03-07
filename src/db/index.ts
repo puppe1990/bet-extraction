1f0fe99df64ce3526fceed2bc75cb4f806f32ca1
puppe1990 <matheus.puppe90@hotmail.com>
2026-03-07 18:25:17 -0300

    Fix Netlify libsql runtime import

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,5 @@
-import { createClient } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client/http";
+import { drizzle } from "drizzle-orm/libsql/http";
 import { serverEnv } from "#/env.server";
 import * as schema from "./schema";
 
